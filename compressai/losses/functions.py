757b6bbdcb83dcd10baa5393e6d617b8d9d888af
abderahmen01 <101531115+abderahmen01@users.noreply.github.com>
2025-06-10 19:35:29 +0100

    Update functions.py

--- compressai/losses/functions.py
+++ compressai/losses/functions.py
@@ -9,28 +9,28 @@ from .percentile import Percentile
 import cv2
 import numpy as np
 
+
 def upsample(img, odd, filt):
     img = F.pad(img, (1, 1, 1, 1), mode='replicate')
     h = 2 * img.shape[2]
     w = 2 * img.shape[3]
-    if img.is_cuda:
-        o = torch.zeros([img.shape[0], img.shape[1], h, w], device=img.get_device())
-    else:
-        o = torch.zeros([img.shape[0], img.shape[1], h, w])
+    # Create tensor on the same device as input
+    o = torch.zeros([img.shape[0], img.shape[1], h, w], device=img.device)
     o[:, :, 0:h:2, 0:w:2] = 4 * img
     o = F.conv2d(o, filt, padding=math.floor(filt.shape[2] / 2))
     o = o[:, :, 2:h - 2 - odd[0], 2:w - 2 - odd[1]]
 
     return o
 
+
 def downsample(img, filt):
     pad = math.floor(filt.shape[2]/2)
-    # print(img.shape)
     img = F.pad(img, (pad, pad, pad, pad), mode='replicate')
     o = F.conv2d(img, filt)
     o = o[:, :, :img.shape[2]:2, :img.shape[3]:2]
 
     return o
+
 
 def laplacian_pyramid_s(img, n_lev, filt):
     pyr = [0] * n_lev  # [0, 0, 0, ...]
@@ -47,11 +47,10 @@ def laplacian_pyramid_s(img, n_lev, filt):
 
     return pyr
 
+
 def nlp(img, n_lev, params):  # 求得原图的拉普拉斯金字塔
         npyr = [0] * n_lev
         img = torch.pow(img, 1 / params['gamma'])
-        # img = torch.log(img)
-        # img = (1e3/math.pi)*torch.atan(img)
         pyr = laplacian_pyramid_s(img, n_lev, params['F1'])
 
         for i in range(0, n_lev-1):
@@ -68,31 +67,30 @@ def nlp(img, n_lev, params):  # 求得原图的拉普拉斯金字塔
 
         return npyr
 
+
 class NLPD_Loss(torch.nn.Module):
     def __init__(self):
         super(NLPD_Loss, self).__init__()
         self.params = dict()
         self.params['gamma'] = 2.60
-        # self.params['gamma'] = 10
         self.params['filts'] = dict()
         self.params['filts'][0] = torch.tensor([[0.0400, 0.0400, 0.0500, 0.0400, 0.0400],
                                                 [0.0400, 0.0300, 0.0400, 0.0300, 0.0400],
                                                 [0.0500, 0.0400, 0.0500, 0.0400, 0.0500],
                                                 [0.0400, 0.0300, 0.0400, 0.0300, 0.0400],
                                                 [0.0400, 0.0400, 0.0500, 0.0400, 0.0400]],
-                                                dtype=torch.float)  # torch.Size([5, 5])
-        self.params['filts'][0] = self.params['filts'][0].unsqueeze(0).unsqueeze(0)  # torch.Size([1, 1, 5, 5])
+                                                dtype=torch.float)
+        self.params['filts'][0] = self.params['filts'][0].unsqueeze(0).unsqueeze(0)
 
         self.params['filts'][1] = torch.tensor([[0, 0, 0, 0, 0],
                                                 [0, 0, 0, 0, 0],
                                                 [0, 0, 1, 0, 0],
                                                 [0, 0, 0, 0, 0],
                                                 [0, 0, 0, 0, 0]],
-                                                dtype=torch.float) # torch.Size([5, 5])
-        self.params['filts'][1] = self.params['filts'][1].unsqueeze(0).unsqueeze(0)  # torch.Size([1, 1, 5, 5])
+                                                dtype=torch.float)
+        self.params['filts'][1] = self.params['filts'][1].unsqueeze(0).unsqueeze(0)
 
-        self.params['sigmas'] = torch.tensor([0.1700, 4.8600], dtype=torch.float)  # torch.Size([2])
-        # self.params['sigmas'] = torch.tensor([0.177, 20], dtype=torch.float)
+        self.params['sigmas'] = torch.tensor([0.1700, 4.8600], dtype=torch.float)
 
         self.params['F1'] = torch.tensor([[0.0025, 0.0125, 0.0200, 0.0125, 0.0025],
                                           [0.0125, 0.0625, 0.1000, 0.0625, 0.0125],
@@ -100,7 +98,7 @@ class NLPD_Loss(torch.nn.Module):
                                           [0.0125, 0.0625, 0.1000, 0.0625, 0.0125],
                                           [0.0025, 0.0125, 0.0200, 0.0125, 0.0025]],
                                           dtype=torch.float)
-        self.params['F1'] = self.params['F1'].unsqueeze(0).unsqueeze(0)  # torch.Size([1, 1, 5, 5])
+        self.params['F1'] = self.params['F1'].unsqueeze(0).unsqueeze(0)
 
         self.exp_s = 2.00
         self.exp_f = 0.60
@@ -113,14 +111,13 @@ class NLPD_Loss(torch.nn.Module):
         l_img = cali_ldr
 
         if n_lev is None:
-            n_lev = math.floor(math.log(min(h_img.shape[2:]), 2)) - 2  # 求得金字塔的层数
-            # print('nlp_n_lev: ', n_lev)
+            n_lev = math.floor(math.log(min(h_img.shape[2:]), 2)) - 2
         filts_0 = self.params['filts'][0]
         filts_1 = self.params['filts'][1]
         sigmas = self.params['sigmas']
         F1 = self.params['F1']
 
-        # Get device from input tensor
+        # Move tensors to input device
         device = h_img.device
         filts_0 = filts_0.to(device)
         filts_1 = filts_1.to(device)
@@ -132,7 +129,7 @@ class NLPD_Loss(torch.nn.Module):
         self.params['sigmas'] = sigmas
         self.params['F1'] = F1
 
-        h_pyr = nlp(h_img, n_lev, self.params)  # default n_lev = 5
+        h_pyr = nlp(h_img, n_lev, self.params)
         l_pyr = nlp(l_img, n_lev, self.params)
 
         dis = []
@@ -146,6 +143,7 @@ class NLPD_Loss(torch.nn.Module):
         loss = torch.pow(torch.mean(dis, dim=-1), 1. / self.exp_f)
 
         return loss.mean()
+
 
 class LDR_Seq(torch.nn.Module):
     def __init__(self):
@@ -198,6 +196,7 @@ class LDR_Seq(torch.nn.Module):
             ek_value.append(ek)
         return result, ek_value
 
+
 class LDR_Seq_out(torch.nn.Module):
     def __init__(self):
         super(LDR_Seq_out, self).__init__()
@@ -210,6 +209,7 @@ class LDR_Seq_out(torch.nn.Module):
         #img[img < 0] = min_v
         number = len(ek_value)
 
+
         result = []
         for i in range(number):
             ek = ek_value[i]
@@ -219,6 +219,7 @@ class LDR_Seq_out(torch.nn.Module):
 
             result.append(imgP)
         return result
+
 
 class hdrMetric(torch.nn.Module):
     def __init__(self):
@@ -240,6 +241,7 @@ class hdrMetric(torch.nn.Module):
         loss = torch.sum(torch.stack(Q))
         return loss
 
+
 def num_to_string(num):
     numbers = {
         'banding': [1.070275272, 0.4088273932, 0.153224308, 0.2520326168, 1.063512885, 1.14115047, 521.4527484],
@@ -249,6 +251,7 @@ def num_to_string(num):
     }
 
     return numbers.get(num, None)
+
 
 def PU21_encoding(Y):
     # epsilon = 1e-5
@@ -261,8 +264,10 @@ def PU21_encoding(Y):
     V = torch.clip(value, 0, 1e16)
     return V
 
+
 def psnr(a: torch.Tensor, b: torch.Tensor, max_val: int = 1) -> float:
     return 20 * math.log10(max_val) - 10 * torch.log10((a - b).pow(2).mean())
+
 
 def color_reproduce(ldr, ref_hdr, hsv_ldr_hat, hsv_target_hdr):
     v_hdr = hsv_target_hdr[:, 2, :, :]
@@ -272,6 +277,7 @@ def color_reproduce(ldr, ref_hdr, hsv_ldr_hat, hsv_target_hdr):
     ldr[:, 0, :, :] = torch.pow(ref_hdr[:, 0, :, :] / v_hdr, 0.6) * v_ldr  # b_ldr
 
     return ldr
+
 
 class BGR_HSV(nn.Module):
     """
